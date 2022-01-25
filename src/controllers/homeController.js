2e1dd4d2de50309ad6f83749f322ea96971a1a72
darinchakalov <darinchakalov@hotmail.com>
2022-01-25 07:30:54 +0200

    home page render courses

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,7 +1,16 @@
 const router = require("express").Router();
 
-const renderHomePage = (req, res) => {
-	res.render("home");
+const courseServices = require("../services/courseServices.js");
+
+const renderHomePage = async (req, res) => {
+	try {
+		let courses = await courseServices.getAll();
+		console.log(courses);
+		res.render("home", { courses });
+	} catch (error) {
+		res.locals.error = error;
+		res.render("home");
+	}
 };
 
 router.get("/", renderHomePage);
--- src/services/courseServices.js
+++ src/services/courseServices.js
@@ -4,8 +4,13 @@ const create = function (title, description, imageUrl, isPublic) {
 	return Course.create({ title, description, imageUrl, isPublic: isPublic });
 };
 
+const getAll = function () {
+	return Course.find().lean();
+};
+
 const courseService = {
 	create,
+	getAll,
 };
 
 module.exports = courseService;
